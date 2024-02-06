8508a35cff291d61da8bfb02bc812b939a4fc53b
Swathi.Vuray <swathi.vuray@rev.io>
2024-02-06 15:31:21 -0500

    interim commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import LoginForm from "./components/loginForm/LoginForm";
 import MorseCodeVisualizer from "./components/morseCodeVisualizer/MorseCodeVisualizer";
 
 const App = () => {
   return (
+    <>
+    <LoginForm/>
     <MorseCodeVisualizer/>
+    </>
+    
   );
 }
 
